fff4307720d7de1d3b30c200b009ceb3e0ebf09a
unknown <patrickkeita80@yahoo.com>
2023-03-21 05:10:33 -0700

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-//eslint-disable-next-line
+// eslint-disable-next-line
 class App extends Component {
   render() {
     return (
       <h1 className="h1">hello world</h1>
-    )
+    );
   }
-};
+}
 
 export default App;
-
