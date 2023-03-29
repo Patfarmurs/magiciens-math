a772ed20928812436cb737cf62b2210f36aeeab1
unknown <patrickkeita80@yahoo.com>
2023-03-29 04:35:01 -0700

    Add Home.js file

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="page-container">
+    <h2 className="page-title">Welcome to our page!</h2>
+    <p className="homepage-content">
+      Feel free to explore this page. Click on Calculator link to do some good &apos;ol Math!
+      Or go to the Quote page, where you&apos;ll find an inspirational  quote.
+    </p>
+  </div>
+);
+
+export default Home;
